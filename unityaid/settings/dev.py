4c58a14afe6ddd3d5e8c19f4b4808a81caefdb2a
jobet1995 <jobetcasquejo221@gmail.com>
2025-11-11 12:31:45 +0800

    Initialize

---
+++ unityaid/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-f2v(@gvik_+rk#egj$^-+_@k1y%jg22z$occzaslc5&in$!0vo"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
